64ec5753af3280c35f62e59e5a348c1ee908cfcf
m1m1sS <33864673+m1m1sS@users.noreply.github.com>
2020-03-11 14:08:55 +0200

    Add files via upload
    initial

---
+++ src/components/ClassComponent.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+class ClassComponent extends Component {
+    render() {
+        return <div className="alert alert-info">
+            <i className="fa fa-cube mr-3"></i>Class Component Example
+        </div>
+    }
+}
+
+export default ClassComponent;
---
+++ src/components/FunctionalComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FunctionalComponent = () => {
+    return <div className="alert alert-secondary">
+        <i className="fa fa-cube mr-3"></i>Functional Component Example
+    </div>
+};
+
+export default FunctionalComponent;
